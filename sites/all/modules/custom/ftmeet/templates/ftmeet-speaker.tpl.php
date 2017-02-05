84a4afe0e11600f3de2fade08f00a84638a1eea9
Abhai Sasidharan <abhaisasidharan@gmail.com>
2017-02-05 17:19:32 +0530

    Session, speaker

--- sites/all/modules/custom/ftmeet/templates/ftmeet-speaker.tpl.php
+++ sites/all/modules/custom/ftmeet/templates/ftmeet-speaker.tpl.php
@@ -9,5 +9,7 @@
     <img src="<?php print file_create_url($speaker['image']); ?>" width="100%"/>
   </div>
   <p class="speaker-description"><?php print $speaker['description']; ?></p>
-  <h4 class="speaker-facebook"><?php print $speaker['facebook']; ?></h4>
+  <?php if ($speaker['facebook']): ?>
+    <h4 class="speaker-facebook"><?php print $speaker['facebook']; ?></h4>
+  <?php endif ?>
 </div>
