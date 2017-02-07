ebecc17dce4f906409bed0545118797b1a27faeb
Abhai Sasidharan <abhaisasidharan@gmail.com>
2017-02-08 05:02:36 +0530

    P tag

--- sites/all/modules/custom/ftmeet/templates/ftmeet-speaker.tpl.php
+++ sites/all/modules/custom/ftmeet/templates/ftmeet-speaker.tpl.php
@@ -8,7 +8,7 @@
   <div class="speaker-image">
     <img src="<?php print file_create_url($speaker['image']); ?>" width="100%"/>
   </div>
-  <p class="speaker-description"><?php print $speaker['description']; ?></p>
+  <?php print $speaker['description']; ?>
   <?php if ($speaker['facebook']): ?>
     <h4 class="speaker-facebook"><?php print $speaker['facebook']; ?></h4>
   <?php endif ?>
