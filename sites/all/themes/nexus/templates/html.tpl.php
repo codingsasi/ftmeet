7a177bf30bf4ad9bda0405753d607632762b402e
Abhai Sasidharan <abhaisasidharan@gmail.com>
2017-02-08 00:38:25 +0530

    Facebook tracking

--- sites/all/themes/nexus/templates/html.tpl.php
+++ sites/all/themes/nexus/templates/html.tpl.php
@@ -5,6 +5,24 @@
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
 <?php print $scripts; ?>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1845646775718905',
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'nexus') . '/js/html5.js'; ?>"></script><![endif]-->
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
